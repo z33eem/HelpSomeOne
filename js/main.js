1d06e3b698073de1b4c6400a42dbae86e7e6e6fc
Al-Z3eem <johndoe@example.com>
2018-07-01 03:06:46 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    
+    $(".show-edit-profile .edit-name span").on("click", function(){
+        $(".Edit_form").fadeIn(600);
+    })
+    
+    $(".Edit_form .close").on("click", function(){
+        $(".Edit_form").fadeOut(600);
+    })
+    
+    
+    
+    
+})
