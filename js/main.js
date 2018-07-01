c2a2458830c5c3d33fe3f30aa2054e38f586a960
Al-Z3eem <johndoe@example.com>
2018-07-01 03:11:06 +0300

    Editin The Speed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(function(){
     
     $(".show-edit-profile .edit-name span").on("click", function(){
-        $(".Edit_form").fadeIn(600);
+        $(".Edit_form").fadeIn(800);
     })
     
     $(".Edit_form .close").on("click", function(){
-        $(".Edit_form").fadeOut(600);
+        $(".Edit_form").fadeOut(800);
     })
     
     
